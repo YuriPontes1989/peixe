0c75d20ac16ae1c0fb86459a37e70a7d4fccc53e
Aluno <aluno@LI.LOCAL>
2022-05-27 22:21:12 -0300

    Initial commit

---
+++ Assets/Flock.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockManager myManager;
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(myManager.minSpeed,
+           myManager.maxSpeed);
+    }
+    void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+}
